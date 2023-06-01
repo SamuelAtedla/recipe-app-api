a8d7294186fc872a7b728f9aba1a00d4a85b4be0
Samuel Assefa <assefa.samuel@gmail.com>
2023-06-01 17:36:04 +0300

    set up admin page fieldset customization,
    this willmake the link work

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,9 @@
 """ Django admin customization"""
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# for language translation
+from django.utils.translation import gettext_lazy as _
+
 from core import models
 
 
@@ -8,6 +11,19 @@ class UserAdmin(BaseUserAdmin):
     """define the admin pages for users."""
     ordering = ["id"]
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        )
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -12,10 +12,13 @@ class AdmiSiteTests(TestCase):
         """Create user and client"""
         self.client = Client()
         self.admin_user = get_user_model().object.create_superuser(
-            email='admin@example.com', password='testpass123',)
+            email='admin@example.com',
+            password='testpass123',)
         self.client.force_login(self.admin_user)
         self.user = get_user_model().object.create_user(
-            email='user@example.com', password='testpass123', name='Test User',)
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',)
 
     def test_user_list(self):
         """Test usera are listed on page."""
@@ -24,3 +27,10 @@ class AdmiSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the user page works."""
+        url = reverse('admin:core_user_change', args=self.user.id)
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
