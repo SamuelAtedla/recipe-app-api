c7649344066f5b7a90a0b0fdaf450f0ffea4db78
Samuel Assefa <assefa.samuel@gmail.com>
2023-06-01 17:41:26 +0300

    wait_for_db break for infinite loop, to see later

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,16 +15,16 @@ class Command(BaseCommand):
 
         self.stdout.write('waiting for database to start ...')
         db_up = False
-        trial_count=0;
+        trial_count = 0
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                if trial_count>8 :
+                if trial_count > 8:
                     break
                 self.stdout.write(
                     'database unavilable, wait for 1 second...')
                 time.sleep(1)
-                trial_count+=1
+                trial_count += 1
         self.stdout.write(self.style.SUCCESS('Database available'))
