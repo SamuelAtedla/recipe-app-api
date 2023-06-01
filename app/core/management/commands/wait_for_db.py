b3a659b43f4ccff64aa1fe2122d000c9d53a9310
Samuel Assefa <assefa.samuel@gmail.com>
2023-06-01 14:51:38 +0300

    model testing and model defined

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,15 +11,20 @@ class Command(BaseCommand):
     """django command to wait for database"""
 
     def handle(self, *args, **options):
-        ''' handles checking of database up'''
+        """ handles checking of database up """
+
         self.stdout.write('waiting for database to start ...')
         db_up = False
+        trial_count=0;
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
+                if trial_count>8 :
+                    break
                 self.stdout.write(
-                    'database unavilable, wait for 1 second for retry')
+                    'database unavilable, wait for 1 second...')
                 time.sleep(1)
+                trial_count+=1
         self.stdout.write(self.style.SUCCESS('Database available'))
