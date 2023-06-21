e14a08fdfddba57eabc868c65bac20b208ae48a7
Samuel Assefa <assefa.samuel@gmail.com>
2023-06-21 15:21:43 +0300

    done recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,6 +10,7 @@ class ModelTest(TestCase):
     """test models"""
 
     def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -21,7 +22,7 @@ class ModelTest(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test email is normalized for new users"""
+        """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
@@ -33,12 +34,12 @@ class ModelTest(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """test creating a user without email raises error"""
+        """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_super_user(self):
-        """test creating a super user"""
+        """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
